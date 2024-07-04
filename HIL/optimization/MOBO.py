40643ba1e7706178acc4c56f488b57119288f3db
vishalvaka <vishalvaka@gmail.com>
2024-07-04 18:25:16 -0500

    plot weights and create base models. fixes to model creation in MOBO

--- HIL/optimization/MOBO.py
+++ HIL/optimization/MOBO.py
@@ -206,9 +206,11 @@ class MultiObjectiveBayesianOptimization(object):
             
             for i in range(train_y.shape[-1]):
                 train_objective = train_y[:, i]
-                models.append(
-                    SingleTaskGP(train_x, train_objective.unsqueeze(-1))
-                )
+                # models.append(
+                #     SingleTaskGP(train_x, train_objective.unsqueeze(-1))
+                # )
+                models.append(SingleTaskGP(train_x, train_objective.unsqueeze(-1), covar_module=ScaleKernel(
+                    base_kernel=RBFKernel(ard_num_dims=self.x_dim)), likelihood=GaussianLikelihood()))
             model = ModelListGP(*models)
             mll = SumMarginalLogLikelihood(model.likelihood, model)
             fit_gpytorch_model(mll)
@@ -340,7 +342,10 @@ class MultiObjectiveBayesianOptimization(object):
             for obj_index in range(num_objectives):
                 model = SingleTaskGP(x, y[:, obj_index].unsqueeze(-1), covar_module=ScaleKernel(
                     base_kernel=RBFKernel(ard_num_dims=self.x_dim)))
+
                 model.likelihood = GaussianLikelihood()
+
+                # model = SingleTaskGP(x, y[: obj_index].unsqueeze(-1))
                 model.to(self.device)
                 model.Y_mean = y.mean(dim=-2, keepdim=True)
                 model.Y_std = y.std(dim=-2, keepdim=True)
